4fb79cace7130ceff9bb1e3c52fd19d0cd012b65
Artem Tebenkov <artem.tebenkov@icloud.com>
2026-09-15 15:35:49 +0300

    Update Package@swift-5.9.swift

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Kingfisher",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v15),
         .macOS(.v10_15),
         .tvOS(.v13),
         .watchOS(.v6),
