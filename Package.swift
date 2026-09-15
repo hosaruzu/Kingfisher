0c62a75f51fd6fc81cbdb921836b928b9cd8ee1a
Artem Tebenkov <artem.tebenkov@icloud.com>
2026-09-15 15:08:16 +0300

    Update Package.swift
    Update target version to 15+

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Kingfisher",
-    platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
+    platforms: [.iOS(.v15), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
     products: [
         .library(name: "Kingfisher", targets: ["Kingfisher"])
     ],
